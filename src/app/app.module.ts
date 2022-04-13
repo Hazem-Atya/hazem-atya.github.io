6a7b3bc73132745466045a342a68b2ddc2f1a323
Hazem-Atya <hazematya1@gmail.com>
2022-04-13 12:46:17 +0100

    divided into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroSectionComponent } from './pages/hero-section/hero-section.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroSectionComponent,
-    SidebarComponent
+    SidebarComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
